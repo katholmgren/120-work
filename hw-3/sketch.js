5c34304fdf92c785f3bd7d17fdb3a426979b176f
Katherine <holmgrenkatherine@gmail.com>
2018-02-06 21:21:15 -0700

    Finished sketch.js file for hw-3

---
+++ hw-3/sketch.js
@@ -0,0 +1,17 @@
+  // Setting up the function
+  function setup(){
+    // Creating a canvas 800px wide and 500 px high
+    createCanvas( 800, 500 );
+
+    // Making the background teal
+    background( 'teal' );
+
+  }
+
+  // Declaring the draw function
+  function draw(){
+    // Printing the text "Hello World!" on the canvas
+    // Positioning the text at ( x: 30, y: 40 )
+    text( "Hello World!", 30, 40 )
+    
+  }
