bebce20f7d594731a12a13a05f8fd83bcfbb48f3
Katherine <holmgrenkatherine@gmail.com>
2018-04-05 22:45:49 -0600

    colored fish, added bubbles

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,21 +1,13 @@
 // School of Fish
 
-// sa means "starting at"
-var sa = [];
+
+let size = [];
 
 function setup(){
   createCanvas( windowWidth, windowHeight );
   frameRate(1);
-  sa = [random (width), random (width), random (width), random (width), random (width)];
 
 }
-
-
-
-//let yPos = [60, 20, 200, 330, 100];
-//let xPos = 0;
-//let xPos = [ 50, 100, 70, 300, 200];
-//let size = [1, .5, .2, 2, 1];
 
 function draw(){
   background( 50, 100, 255 );
@@ -25,22 +17,30 @@ function draw(){
   for( let idx = 0; idx < numFish; idx++ ) {
       // draw some boxes!
       fish( random(0, width) , random(0, height), 1, 1 );
-  }
+    }
 
-  //sa = (sa + 10) % windowWidth;
+
+  for( let i = 0; i < width; i+= 10 ) {
+    bubbles(i, random(0, height));
+}
 }
 
-function drawFish(sa) {
-  let fishPosX = fishPosX + 10;
+function bubbles(bubbleX, bubbleY){
+    push();
+    translate(bubbleX, bubbleY);
+    strokeWeight(.5 );
+    fill('rgba( 150, 0, 150, 0.25 )');
+    ellipse( 0, 0, 10 );
+    pop();
 }
 
-function fish(fishPosX, fishPosY, scaleX, scaleY ){
+function fish(fishPosX, fishPosY ){
   push();
   translate(fishPosX, fishPosY);
-  scale(scaleX, scaleY);
-  fill( 240, 50, 150 );
+  fill('rgba( 255, 50, 150, 0.5 )');
   triangle( -40, 0, -60, 20, -60, -20 );
   quad( 10, -40, 20, -20, -10, -20, -50, -40 );
+  fill( 240, 50, 150 );
   ellipse( 0, 0, 100, 50 );
   strokeWeight(5);
   point( 35, 0 );
