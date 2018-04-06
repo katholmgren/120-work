f338b7f6835e3f262004da09e2a3b8e85d994bb4
Katherine <holmgrenkatherine@gmail.com>
2018-04-05 23:04:00 -0600

    Finished sketch

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,48 +1,52 @@
 // School of Fish
 
-
-let size = [];
+// Make an array to cycle size of fishes
+let size = [ .75, 1, 1, 1.2, .75, 1, 1, 1 ];
 
 function setup(){
   createCanvas( windowWidth, windowHeight );
-  frameRate(1);
-
+  frameRate( 1 );
 }
 
 function draw(){
   background( 50, 100, 255 );
-
-  let numFish = 8; //sets limit on number of fish created
+  // Set limit on number of fish created
+  let numFish = 8;
 
   for( let idx = 0; idx < numFish; idx++ ) {
-      // draw some boxes!
-      fish( random(0, width) , random(0, height), 1, 1 );
-    }
-
+    // Draw 8 fish
+    // in different spots
+    // in different sizes
+    fish( random(0, width) , random(0, height), size[idx] );
+  }
 
   for( let i = 0; i < width; i+= 10 ) {
-    bubbles(i, random(0, height));
-}
+    // Draw lots of random bubbles
+    bubbles( i, random(0, height) );
+  }
 }
 
-function bubbles(bubbleX, bubbleY){
+// Define bubbles
+function bubbles( bubbleX, bubbleY ){
     push();
-    translate(bubbleX, bubbleY);
-    strokeWeight(.5 );
-    fill('rgba( 150, 0, 150, 0.25 )');
+    translate( bubbleX, bubbleY );
+    strokeWeight( .5 );
+    fill( 'rgba( 150, 0, 150, 0.25 )' );
     ellipse( 0, 0, 10 );
     pop();
 }
 
-function fish(fishPosX, fishPosY ){
+// Define fish
+function fish( fishPosX, fishPosY, sizeX, sizeY ){
   push();
-  translate(fishPosX, fishPosY);
-  fill('rgba( 255, 50, 150, 0.5 )');
+  translate( fishPosX, fishPosY );
+  scale( sizeX, sizeY );
+  fill( 'rgba( 255, 50, 150, 0.5 )' );
   triangle( -40, 0, -60, 20, -60, -20 );
   quad( 10, -40, 20, -20, -10, -20, -50, -40 );
   fill( 240, 50, 150 );
   ellipse( 0, 0, 100, 50 );
-  strokeWeight(5);
+  strokeWeight( 5 );
   point( 35, 0 );
   pop();
 }
